63331e5743acfea9e4565c77d72896bcc8555843
justin G <justin.golshir@gmail.com>
2018-05-21 12:57:19 -0400

    Updated app with old build

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
-const express = require("express");
+const express = require('express')
 const path = require("path");
-const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 3001;
-const app = express();
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const passport = require('./passport')
+const mongoose = require("mongoose");
+//const routes = require("./routes");
+const app = express()
+const PORT = process.env.PORT || 3000
 
 // Define middleware here
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,13 +18,42 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+// Connect to the Mongo DB
+
+var dbConnection = mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/create-react-express");
+
+// Code I'm not sure about for session storage
+app.use(
+	session({
+		secret: process.env.APP_SECRET || 'this is the default passphrase',
+		store: new MongoStore({ mongooseConnection: mongoose.connection }),
+		resave: false,
+		saveUninitialized: false
+	})
+)
+
+// ===== Passport ====
+app.use(passport.initialize())
+app.use(passport.session()) // will call the deserializeUser
+
 // Define API routes here
 
 // Send every other request to the React app
 // Define any API routes before this runs
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
+/* Express app ROUTING */
+app.use('/auth', require('./routes/auth'))
+
+// ====== Error handler ====
+app.use(function(err, req, res, next) {
+	console.log('====== ERROR =======')
+	console.error(err.stack)
+	res.status(500)
+})
+
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
